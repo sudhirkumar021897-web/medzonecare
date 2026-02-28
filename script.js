6413f2d4d7f1fff5928b39b145a8794a1cd6614f
sudhirkumar021897-web <sudhir.kumar021897@gmail.com>
2026-03-01 01:55:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-// Preloader
-window.addEventListener("load", function(){
-    document.getElementById("preloader").style.display = "none";
-});
-
-// Scroll header effect
-window.addEventListener("scroll", function(){
-    document.querySelector("header").classList.toggle("scrolled", window.scrollY > 50);
-});
-
 // Counter animation
-const counters = document.querySelectorAll(".counter h3");
-counters.forEach(counter => {
-    counter.innerText = '0';
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target');
-        const c = +counter.innerText;
-        const increment = target / 100;
-        if(c < target){
-            counter.innerText = `${Math.ceil(c + increment)}`;
-            setTimeout(updateCounter,20);
-        } else {
-            counter.innerText = target;
-        }
-    };
-    updateCounter();
-});
+const counters = document.querySelectorAll(".stats h3");
 
-// FAQ toggle
-document.querySelectorAll(".faq-item").forEach(item=>{
-    item.addEventListener("click", ()=>{
-        const answer = item.querySelector(".faq-answer");
-        answer.style.display = answer.style.display === "block" ? "none" : "block";
-    });
+counters.forEach(counter=>{
+counter.innerText="0";
+const update=()=>{
+const target=+counter.getAttribute("data-target");
+const c=+counter.innerText;
+const inc=target/100;
+
+if(c<target){
+counter.innerText=Math.ceil(c+inc);
+setTimeout(update,20);
+}else{
+counter.innerText=target;
+}
+};
+update();
 });
